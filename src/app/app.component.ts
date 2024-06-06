02dc8c8e7682c159bb083218596fefbfb069ebdc
Erica Torrico <ericatorrico@gmail.com>
2024-06-06 17:50:57 -0300

    Add iteration, method and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-end';
+
+  movies = [
+    {
+      title: 'Spider-Man',
+      launchDate: new Date(),
+      price: 1400.99
+    },
+    {
+      title: 'Moana',
+      launchDate: new Date(2016-11-14),
+      price: 1230.99
+    },
+    {
+      title: 'Spider-Man 2',
+      launchDate: new Date(2021-6-29),
+      price: 1400.99
+    }
+  ]
+
+  doubleNumber(value:number):number {
+    return value*2
+  }
 }
