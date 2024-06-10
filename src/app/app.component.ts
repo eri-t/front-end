ebefa68ed76202dcdbef147df612914500253d97
Erica Torrico <ericatorrico@gmail.com>
2024-06-10 11:09:23 -0300

    Add movies list component, add loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.showingMovies = [
+        {
+          title: 'Spider-Man',
+          launchDate: new Date(),
+          price: 1400.99
+        },
+        {
+          title: 'Moana',
+          launchDate: new Date("2016-11-14"),
+          price: 1230.99
+        }
+      ],
+        this.nextMovies = [
+          {
+            title: 'Spider-Man Far from home',
+            launchDate: new Date("2024-12-24"),
+            price: 1900.99
+        },
+        {
+          title: 'Inception',
+          launchDate: new Date("2025-8-25"),
+          price: 1800.99
+        },
+        {
+          title: 'Rocky',
+          launchDate: new Date("2025-7-12"),
+          price: 1700.99
+        }
+      ]
+    }, 3000);
+  }
   title = 'front-end';
-
-  movies = [
-    {
-      title: 'Spider-Man',
-      launchDate: new Date(),
-      price: 1400.99
-    },
-    {
-      title: 'Moana',
-      launchDate: new Date(2016-11-14),
-      price: 1230.99
-    },
-    {
-      title: 'Spider-Man 2',
-      launchDate: new Date(2021-6-29),
-      price: 1400.99
-    }
-  ]
+  showingMovies;
+  nextMovies;
 
   doubleNumber(value:number):number {
     return value*2
---
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-list',
+  templateUrl: './movies-list.component.html',
+  styleUrls: ['./movies-list.component.css']
+})
+export class MoviesListComponent implements OnInit {
+  constructor() { }
+  @Input()
+  movies;
+
+  ngOnInit(): void { }
+
+  remove(index: number): void {
+    this.movies.splice(index, 1)
+  }
+}
